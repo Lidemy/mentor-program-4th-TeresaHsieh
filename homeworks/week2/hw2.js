8c24de7ec525e35859d60683ea8bed2d81e19758
Teresa Hsieh <teresahsieh850506@gmail.com>
2020-06-28 21:54:43 +0800

    feat(week2): finish hw1-hw6

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,11 @@
 function capitalize(str) {
-  
+  let englishCharacters = /^[A-Za-z]+$/;
+  let firstLetter = str.charAt(0);
+  if (firstLetter.match(englishCharacters)){
+    return firstLetter.toUpperCase() + str.slice(1);
+  } else {
+    return str; 
+  }
 }
 
 console.log(capitalize('hello'));
--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,10 @@
 function reverse(str) {
-  
+  let strLength = str.length;
+  let reversedStrArray = [];
+  for (let i = strLength - 1; i >= 0; i--) {
+    reversedStrArray.push(str[i]);
+  }
+  console.log(reversedStrArray.join(''));
 }
 
 reverse('hello');
